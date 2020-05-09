cd6c51a19ae3303b7f51a869294fc350e3405d7b
Ramon Medeiros <ramon.medeiros@recordunion.com>
2020-05-09 16:20:32 +0200

    Mock apis

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,14 +4,14 @@ from unittest.mock import patch, MagicMock
 import scoreboard
 
 @pytest.fixture
-
-def client():
+@patch('scoreboard.database.psycopg2')
+@patch('scoreboard.slackApi.WebClient')
+def client(webclient, db):
     app = scoreboard.startApp()
     app.config['TESTING'] = True
 
-    with app.test_client() as client:
-        yield client
-
+    return app.test_client()
 
 def test_app(client):
     assert client.get("/").status_code == 404
+
